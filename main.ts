1d8ddde32c4b17a7041e56d1176a96b690d6701e
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2025-02-05 21:53:43 -0500

    Enough for today

--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
-//% color=190 weight=100 icon="\uf1ec" block="Better NeoPixel"
-namespace betterneopixel {
-    //% block
-    export function createGraph() {
-        
+//% color="#AA278D" weight=100 icon="\uf0eb" block="Better NeoPixel"
+namespace betterNeopixel {
+    enum Style {
+        //% block="outside to inside"
+        oti = 0,
+        //% block="inside to outside"
+        ito = 1,
+        //% block="left to right"
+        ltr = 2,
+        //% block="right to left"
+        rtl = 3,
+    }
+    
+    //% block="create graph with $np|using value $value|capped at $max|using style $style"
+    export function createGraph(np: neopixel.Strip,
+    value: number, max: number, style: Style) {
+        let percent = value / max
+        let half_length = Math.floor(np.length() / 2)
+        let np_to_light = Math.round(
+            Math.min(half_length, percent * half_length)
+        )
+
+
     }
 }
